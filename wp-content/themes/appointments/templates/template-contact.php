71ec2405754e9dbb70dae0ac9bfbd7cb1e6ca862
Nguyen hiep <hiep.nguyen@hdwebsoft.co>
2018-07-09 11:22:58 +0700

    [change] update google map

--- wp-content/themes/appointments/templates/template-contact.php
+++ wp-content/themes/appointments/templates/template-contact.php
@@ -4,6 +4,9 @@ get_header();
 global $post;
 ?>
 <section id="content">
+    <?php if(get_field('google_map')):?>
+    <div class="googlemap"><?php echo get_field('google_map')?></div>
+    <?php endif; ?>
     <?php echo apply_filters('the_content', $post->post_content); ?>
 </section>
 <?php get_footer(); ?>
