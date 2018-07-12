92ebcad8c8d7803c83cc98f067d7ac1e26364b35
Nguyen Hiep <minhhiep.q@gmail.com>
2018-07-12 22:41:39 +0700

    [change] update js page

--- wp-content/themes/appointments/assets/js/main.js
+++ wp-content/themes/appointments/assets/js/main.js
@@ -1,39 +1,44 @@
 $(function() {
-    indexPage();
-    showHideTitle();
+  indexPage();
+  showHideTitle();
 
-    $(".burger-menu").click(function () {
-        $(this).toggleClass("menu-on");
-        $('.menu-container').toggleClass('active');
-    });
+  $(".burger-menu").click(function () {
+    $(this).toggleClass("menu-on");
+    $('.menu-container').toggleClass('active');
+  });
 });
 function indexPage() {
-    var indexTitle = $('.index-text');
+  var indexTitle = $('.index-text');
 
-    indexTitle.on('mouseover', function(e) {
-        $('.background').removeClass('active');
-        var target = '.'+e.currentTarget.dataset.background;
-        $(target).addClass('active');
-    });
+  indexTitle.on('mouseover', function(e) {
+    $('.background').removeClass('active');
+    var target = '.'+e.currentTarget.dataset.background;
+    $(target).addClass('active');
+  });
 }
 
 function showHideTitle() {
-    var pageTitle = $('.page-title');
-
-    $(window).scroll(function() {
-        var top = $(window).scrollTop();
-        if (top == 0) {
-            pageTitle.addClass('active');
-            pageTitle.css('visibility', 'visible');
-        } else {
-            if(pageTitle.hasClass('active')) {
-                pageTitle.removeClass('active');
-                pageTitle.delay(300)
-                .queue(function (next) { 
-                    $(this).css('visibility', 'hidden'); 
-                    next(); 
-                });
-            };
-        }
-    });
+  var pageTitle = $('.page-title');
+  pageTitle.on('click', function(){
+    hideTitleLayer(pageTitle);
+  });
+  $(window).scroll(function() {
+    var top = $(window).scrollTop();
+    if (top == 0) {
+      pageTitle.addClass('active');
+      pageTitle.css('visibility', 'visible');
+    } else {
+      hideTitleLayer(pageTitle);
+    }
+  });
+}
+function hideTitleLayer(pageTitle) {
+  if(pageTitle.hasClass('active')) {
+    pageTitle.removeClass('active');
+    pageTitle.delay(300)
+        .queue(function (next) {
+          $(this).css('visibility', 'hidden');
+          next();
+        });
+  };
 }
--- wp-content/themes/appointments/footer.php
+++ wp-content/themes/appointments/footer.php
@@ -3,7 +3,7 @@
 	src="https://code.jquery.com/jquery-2.2.4.min.js"
 	integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
 	crossorigin="anonymous"></script>
-<script src="<?php echo THEME_URL . 'assets/' ?>js/main.js"></script>
+<script src="<?php echo THEME_URL ?>assets/js/main.js?ver=<?php echo STYLE_VERSION ?>"></script>
 <?php wp_footer(); ?>
 </body>
 </html>
