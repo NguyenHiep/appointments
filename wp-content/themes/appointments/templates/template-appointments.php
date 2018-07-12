52617f469501661038b5ff9c3dab66426620d6de
Nguyen Hiep <minhhiep.q@gmail.com>
2018-07-12 23:52:52 +0700

    [change] update plugins new

--- wp-content/themes/appointments/templates/template-appointments.php
+++ wp-content/themes/appointments/templates/template-appointments.php
@@ -3,7 +3,18 @@
 get_header();
 global $post;
 ?>
-<section id="content">
-    <?php echo apply_filters('the_content', $post->post_content); ?>
+<section class="page page--appointments">
+    <div class="row">
+      <div class="col-md-6">
+        <div class="post-desc">
+            <?php if(get_field('instroduction')):?>
+              <?php echo get_field('instroduction')?>
+            <?php endif; ?>
+        </div>
+      </div>
+      <div class="col-md-6">
+          <?php echo apply_filters('the_content', $post->post_content); ?>
+      </div>
+    </div>
 </section>
 <?php get_footer(); ?>
