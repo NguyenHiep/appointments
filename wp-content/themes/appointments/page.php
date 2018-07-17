8dd4a57b98171459d71aeb83a04e7e0f33b56c1a
Nguyen Hiep <minhhiep.q@gmail.com>
2018-07-18 00:26:03 +0700

    [change] update template page

--- wp-content/themes/appointments/page.php
+++ wp-content/themes/appointments/page.php
@@ -1,27 +1,33 @@
 <?php
 get_header();
 if (have_posts()) :
-    $page_id = get_the_ID();
+    global $post;
     ?>
-    <div class="clearfix"></div>
-    <section id="content">
-        <div class="thumbnail-img clearfix">
-            <?php if (has_post_thumbnail()): ?>
-                <img src="<?php echo wp_get_attachment_url(get_post_thumbnail_id($page_id), 'full') ?>" alt=""
-                     class="img-responsive" style="width: 100%;">
-            <?php endif; ?>
-        </div>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <h1 class="text-center mcare-page"><?php the_title() ?></h1>
-                    <div class="content-post">
-                        <?php the_content() ?>
-                    </div>
+    <div class="page">
+        <div class="page__inner">
+            <h1 class="page-title active"><strong><?php the_title() ?></strong></h1>
+            <div class="post-block">
+                <div class="row">
+                    <?php if (has_post_thumbnail()): ?>
+                        <div class="thumbnail-img clearfix col-md-3">
+                            <img src="<?php echo wp_get_attachment_url(get_post_thumbnail_id($post->ID), 'full') ?>"
+                                 alt="thumbnail-img"
+                                 class="img-responsive" style="width: 100%;">
+                        </div>
+                        <div class="col-md-9">
+                            <?php echo apply_filters('the_content', $post->post_content); ?>
+                        </div>
+                    <?php else: ?>
+                        <div class="col-md-12">
+                            <?php echo apply_filters('the_content', $post->post_content); ?>
+                        </div>
+                    <?php endif; ?>
+
                 </div>
             </div>
         </div>
-    </section>
+    </div>
     <?php
 endif;
 get_footer();
+?>
